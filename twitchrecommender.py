c1cc6269bd86d0b1d8cd285685975d9cf8c8ae6a
Mason Saucier <jmsaucier91@gmail.com>
2015-06-10 19:06:48 -0500

    cleaned up recommendation return

--- twitchrecommender.py
+++ twitchrecommender.py
@@ -8,9 +8,17 @@ import ConfigParser
 
 print "Starting matrix load..."
 graphMatrix = marshal.load(open('graphMatrixFilled.dat', 'rb'))
-channelLookupById = marshal.load(open('channelLookupById.dat','rb'))
-liveChannelCache = {}
 
+print "Starting lookup load..."
+channelLookupById = marshal.load(open('channelLookupById.dat','rb'))
+channelLookupByName = {}
+for i in channelLookupById:
+
+    name = channelLookupById[i][1]
+    #print name
+    channelLookupByName[name] = channelLookupById
+
+liveChannelCache = {}
 auth = OAuthSignIn()
 
 config = ConfigParser.RawConfigParser()
@@ -102,24 +110,26 @@ def generateDetailedRecommendationListForUser(follower):
     return recommendations[:10]
 
 def generateRecommendationListForUser(follower):
-    global graphMatrix, channelLookupById
-    #retrieve followers that are live
+    global graphMatrix, channelLookupById, channelLookupByName
+    #retrieve followers and filter to ones that aren't live
+    #we do this because they may not [i]really[/i] like their followed channels that are live
     totalFollowedChannels = getChannelIdsForUserName(follower, max(channelLookupById))
-    followedChannels = filter(lambda x: not x in liveChannelCache, totalFollowedChannels)
-
-    #pair down list for easier analysis
-    if len(followedChannels) > 20:
-        followedChannels = random.sample(totalFollowedChannels, 20)
 
     #they are not in the DB, so we need to generate random recommendations
     if follower == '' or len(totalFollowedChannels) == 0:
-        totalFollowedChannels = random.sample(liveChannelCache, 20)
-        followedChannels = filter(lambda x: not x in liveChannelCache, totalFollowedChannels)
+        totalFollowedChannels = random.sample(channelLookupById, 100)
+
+    followedChannels = filter(lambda x: not channelLookupById[x] in liveChannelCache, totalFollowedChannels)
+    #pair down list for easier analysis
+    if len(followedChannels) > 20:
+        followedChannels = random.sample(followedChannels, 20)
 
 
     uniqueViewerWorkingDict = {}
 
-    #all channels are equal
+    #for now, all channels are created equal
+    #this will change later when viewing multiple days actually counts for something
+    #ie: i watched sevadus for 3 days, therefore he means a lot to me <3
     for i in followedChannels:
         uniqueViewerWorkingDict[i] = 1000
 
@@ -141,14 +151,28 @@ def generateRecommendationListForUser(follower):
                 graphMatrixFollowing = graphMatrix[iId][j] / float(graphMatrix[iId][iId])
                 uniqueViewerCount = uniqueViewerWorkingDict[iId]
                 possibleRecommendations[j] += graphMatrixFollowing * uniqueViewerCount
-        #update_progress((i + 1) / float(len(followedChannels)))
 
-    #filterout the unknown
-    recommendations = [i for i in sorted(enumerate(possibleRecommendations), key=lambda x: x[1] / float(graphMatrix[x[0]][x[0]] + 1), reverse=True)]
+    recommendations = []
+
+    #we need to make sure users that are not logged in aren't completely random
+    #therefore we sort by number of viewers
+    if(follower == ''):
+        recommendations = [i for i in sorted(enumerate(possibleRecommendations), key=lambda x: x[1], reverse=True)]
+    else:
+        recommendations = [i for i in sorted(enumerate(possibleRecommendations), key=lambda x: x[1] / float(graphMatrix[x[0]][x[0]] + 1), reverse=True)]
+
+    #sort and filter recommendations that are not live
     recommendations = filter(lambda x: (not x[0] in totalFollowedChannels) and x[0] in channelLookupById and channelLookupById[x[0]][1] in liveChannelCache,recommendations )
-    recommendations = map(lambda x: channelLookupById[x[0]][1], recommendations)
-    recommendations = map(lambda x: [x,liveChannelCache[x]], recommendations)
-    limitedRecommendation = recommendations[:min(len(recommendations), 50)]
+
+    def recommendation_mapping(x):
+        name = channelLookupById[x[0]][1]
+        game = liveChannelCache[name]
+        return [name, game]
+
+    #call mapping function to our output
+    recommendations = map(recommendation_mapping, recommendations)
+    #limit the number of recommendations, should probably load this number from config
+    limitedRecommendation = recommendations[:min(len(recommendations), 10)]
 
     return limitedRecommendation
 
@@ -156,6 +180,7 @@ print "Starting refresh of channel cache..."
 refreshChannelCache()
 if __name__ == '__main__':
     print "Running recommendations"
-    recommendations = generateRecommendationListForUser('bledahm')
-    for i in recommendations:
-        print i[0], i[1]
+    for j in range(3):
+        recommendations = generateRecommendationListForUser('')
+        for i in range(len(recommendations)):
+            print i,recommendations[i][0], recommendations[i][1]
